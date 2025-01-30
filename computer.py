b6529586923ee53e96dd55fa5fdbc09d47c4b62d
s-bye <kill46923@gmail.com>
2025-01-30 11:55:45 +0600

    Added upgrade_mem() function

--- computer.py
+++ computer.py
@@ -2,14 +2,14 @@ class Computer:
     def __init__(self, psu, cpu, mem, gpu, hard_drive):
         self.psu = psu
         self.cpu = cpu
-        self.mem = mem
+        self.mem = int(mem)
         self.gpu = gpu
         self.hard_drive = hard_drive
 
     def display_info(self):
         print("Computer Information")
         print("CPU Model:", self.cpu)
-        print("Memory Model:", self.mem)
+        print("Memory Size (GB):", self.mem)
         print("GPU Model:", self.gpu)
         print("Hard Drive:", self.hard_drive)
 
--- main.py
+++ main.py
@@ -8,15 +8,29 @@ if __name__ == "__main__":
     for i in range(num):
         print(f"Enter details of the computer №{i+1}")
         cpu = input("CPU Name: ")
-        mem = input("Memory Name: ")
+        mem = int(input("Memory Size(GB): "))
         gpu = input("GPU Name: ")
         hard_drive = input("Hard Drive Name: ")
         psu = input("PSU Name: ")
 
-        comp = Computer(cpu, mem, gpu, hard_drive, psu)
+        comp = Computer(psu, cpu, mem, gpu, hard_drive)
         computers.append(comp)
 
         print(f"Computer information: {i+1}")
         for i, comp in enumerate(computers, 1):
             print(f"Computer {i}:")
             comp.display_info()
+
+    while True:
+        upgrade_choice = input("Would you like to upgrade memory for a computer? (yes/no)\n")
+        if upgrade_choice == "yes":
+            comp_num = int(input(f"Enter number of computer (1-{num}): ")) - 1
+            if 0 <= comp_num < num:
+                additional_mem = int(input("Enter additional memory you would like to add (GB): "))
+                computers[comp_num].upgrade_mem(additional_mem)
+            else:
+                print("Invalid computer number!")
+        elif upgrade_choice == "no":
+            break
+        else:
+            print("Please enter a valid option 'yes' or 'no'!")
